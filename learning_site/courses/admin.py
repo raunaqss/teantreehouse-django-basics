768743dc5f4cf853a895405c7dfc08a69539693a
Raunaq Singh Sahni <raunaq@spacecom.in>
2019-10-05 10:22:53 +0530

    Add model Steps to courses app, link to to the Course model and create and inline admin class

--- learning_site/courses/admin.py
+++ learning_site/courses/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Step
 # Register your models here.
-admin.site.register(Course)
+
+class StepInline(admin.StackedInline):
+    '''Class to help define steps from within the admin form for courses'''
+    model = Step
+
+class CourseAdmin(admin.ModelAdmin):
+    '''This class helps us modify Course model's admin class'''
+    inlines = [StepInline, ]
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Step)
--- learning_site/courses/models.py
+++ learning_site/courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
+    '''Model for a course on our learning site'''
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -9,3 +10,13 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 
+
+class Step(models.Model):
+    '''Model for a step within a course'''
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    order = models.IntegerField(default=0)
+    course = models.ForeignKey(Course, models.CASCADE)
+
+    def __str__(self):
+        return self.title
