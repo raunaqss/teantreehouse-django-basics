943efc11c2301c56c1e975eddddc3fc4c8ccd142
Raunaq Singh Sahni <raunaq@spacecom.in>
2019-10-05 13:14:28 +0530

    Follow video to create a view for step detail, except I wanted to do it by order rather than by primary key

--- learning_site/courses/urls.py
+++ learning_site/courses/urls.py
@@ -3,5 +3,6 @@ from . import views
 # this HAS to be named urlpatterns for it to work, because Django expects it that way of course 
 urlpatterns = [
     path('', views.course_list, name='Course List'),
-    re_path(r'(?P<pk>\d+)', views.course_detail, name='Course Detail')
+    re_path(r'(?P<course_pk>\d+)/(?P<step_order>\d+)', views.step_order_detail, name='Step Detail'),
+    re_path(r'(?P<course_pk>\d+)', views.course_detail, name='Course Detail')
 ]
--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Course
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import Course, Step
 # Create your views here.
 
 def course_list(request):
@@ -7,7 +7,13 @@ def course_list(request):
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', {'courses': courses})
 
-def course_detail(request, pk):
+def course_detail(request, course_pk):
     '''Takes the course primary key from path and shows its details'''
-    course = get_object_or_404(Course, pk=pk)
+    course = get_object_or_404(Course, pk=course_pk)
     return render(request, 'courses/course_detail.html', {'course': course})
+
+def step_order_detail(request, course_pk, step_order):
+    '''Unlike the step detail view in the course, 
+    I want to show the steps by order rather than id'''
+    steps = get_list_or_404(Step, course_id=course_pk, order=step_order)
+    return render(request, 'courses/step_detail.html', {'steps': steps, 'order': step_order})
