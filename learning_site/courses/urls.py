1ef52d1aac647ac505534b31fc7a5f916c340812
Raunaq Singh Sahni <raunaq@spacecom.in>
2019-10-05 11:47:16 +0530

    Add detailed view for course detail

--- learning_site/courses/urls.py
+++ learning_site/courses/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 # this HAS to be named urlpatterns for it to work, because Django expects it that way of course 
-urlpatterns=[
-    path('', views.course_list, name='Course List')
+urlpatterns = [
+    path('', views.course_list, name='Course List'),
+    re_path(r'(?P<pk>\d+)', views.course_detail, name='Course Detail')
 ]
--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -6,4 +6,8 @@ def course_list(request):
     '''Takes request and renders page with list of all courses'''
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', {'courses': courses})
-    
+
+def course_detail(request, pk):
+    '''Takes the course primary key from path and shows its details'''
+    course = Course.objects.get(pk=pk)
+    return render(request, 'courses/course_detail.html', {'course': course})
